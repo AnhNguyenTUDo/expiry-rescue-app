d498603768626532a00a94e0cf1fa826df13ee84
Shiro <phuonganh9301@gmail.com>
2026-05-23 00:52:58 +0200

    fix: fix tooltip to sit on top of header

--- web-app/app/components/ui/Tooltip.vue
+++ web-app/app/components/ui/Tooltip.vue
@@ -4,7 +4,7 @@
     v-if="content && active"
     :id="id"
     role="tooltip"
-    class="absolute z-30 invisible inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-300 bg-gray-700 rounded-lg shadow-sm opacity-0 tooltip"
+    class="absolute z-50 invisible inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-300 bg-gray-700 rounded-lg shadow-sm opacity-0 tooltip"
   >
     {{ content }}
     <div class="tooltip-arrow" data-popper-arrow></div>
