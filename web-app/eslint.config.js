6468aa20ecdc382a05dc7a0a99409f3b74b5d987
Shiro <phuonganh9301@gmail.com>
2026-05-26 20:27:41 +0200

    chore: update eslint config to ignore generated dirs and allow console.error

--- web-app/eslint.config.js
+++ web-app/eslint.config.js
@@ -3,9 +3,12 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
+    ignores: [".nuxt/**", ".output/**", "node_modules/**"],
+  },
+  {
     rules: {
       "no-unused-vars": "warn",
-      "no-console": "warn",
+      "no-console": ["warn", { allow: ["error", "warn"] }],
       "vue/html-self-closing": "off",
       "vue/multi-word-component-names": "off",
     },
