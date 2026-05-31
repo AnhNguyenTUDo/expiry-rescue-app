080203cca1f403cab50e9620784c2814fa936044
Shiro <phuonganh9301@gmail.com>
2026-05-31 13:54:33 +0200

    chore: remove unreachable /products section

--- web-app/app/layouts/default.vue
+++ web-app/app/layouts/default.vue
@@ -49,10 +49,6 @@ const updateSelectedSupermarket = () => {
       supermarketStore.setSelectedSupermarketId(supermarketId)
     }
   }
-  // Check if we're on a product page with supermarketId query param
-  else if (route.path.startsWith('/products/') && route.query.supermarketId) {
-    selectedSupermarket.value = route.query.supermarketId
-    supermarketStore.setSelectedSupermarketId(route.query.supermarketId)
   } else {
     selectedSupermarket.value = ''
     supermarketStore.clearSelectedSupermarket()
